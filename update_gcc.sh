7fc836a07f4565eb2341edb831e3be18b97914c2
roynatech2544 <whiteshell2544@naver.com>
2022-11-18 20:37:37 +0900

    Add script to update GCC toolchain to mine

---
+++ update_gcc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -r prebuilts/gcc/linux-x86/arm/arm-linux-androideabi
+git clone -q https://github.com/roynatech2544/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 prebuilts/gcc/linux-x86/arm/arm-linux-androideabi
